503375b4792bcf8cb80c9d0f2e134fc22cae9490
TnTPixelCombats2 <tratiliwanilotnt21@gmail.com>
2025-04-30 08:59:34 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,1 +1,15 @@
+// * Импорты * //
+import * as room from 'pixel_combats/room';
+import * as basic from 'pixel_combats/basic';
+import * as team from './default_team.js';
+import * as timers from './default_timer.js';
 
+// * Константы * //
+const MaxScores = 6;
+const WaitingModeSeconds = 11;
+const BuildModeSeconds = 31;
+const Game_mode_seconds = timers.timers_seconds_length();
+const end_game_seconds = 6;
+const end_0f_match_seconds = 11;
+const waiting_state_value = 'Waiting';
+const 
